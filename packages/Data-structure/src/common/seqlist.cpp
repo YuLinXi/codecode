187b83929170cfa5dee78df6f8b22a376adc4f3c
yuLinXi <yuLinXi>
2022-08-11 23:11:55 +0800

    :construction: update

--- packages/Data-structure/src/common/seqlist.cpp
+++ packages/Data-structure/src/common/seqlist.cpp
@@ -1,6 +1,6 @@
 #define InitSize 10
+#define ElemType int
 
-typedef int ElemType;
 typedef int Data[];
 typedef struct
 {
@@ -15,7 +15,7 @@ void InitSeqList(SeqList &L)
   L.MaxSize = InitSize;
 }
 
-void IncreateSize(SeqList &L, int len) {
+void IncreaseSize(SeqList &L, int len) {
   int *p = L.data;
   L.MaxSize = L.MaxSize + len;
   L.data = new ElemType[L.MaxSize];
@@ -27,7 +27,47 @@ void IncreateSize(SeqList &L, int len) {
 
 void AddElem(SeqList &L, int value) {
   if (L.length == L.MaxSize) {
-    IncreateSize(L, 10);
+    IncreaseSize(L, 10);
   }
   L.data[L.length++] = value;
+}
+
+bool ListInsert(SeqList &L, int i, ini e) {
+  if (i < 1 || i > L.length + 1) {
+    return false;
+  }
+  if (L.length >= L.MaxSize) {
+    return false;
+  }
+  for (int j = L.length; j >= i; j--) {
+    L.data[j] = L.data[j - 1];
+  }
+  L.data[i - 1] = e;
+  L.length++;
+  return true;
+}
+
+bool ListDelete(SeqList &L, int i, int &e) {
+  if (i < 1 || i > L.length + 1) {
+    return false;
+  }
+  e = L.data[i - 1];
+  for (int j = i - 1; j < L.length - 1; j ++) {
+    L.data[j] = L.data[j + 1];
+  }
+  L.length--;
+  return true;
+}
+
+ElemType GetElem(SeqList &L, int i) {
+  return L.data[i - 1];
+}
+
+int Locate(SeqList &L, int ElemType e) {
+  for (int i = 0; i < L.length; i++) {
+    if (L.data[i] == e) {
+      return i + 1;
+    }
+  }
+  return 0;
 }
--- packages/Data-structure/src/common/sqlist.cpp
+++ packages/Data-structure/src/common/sqlist.cpp
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #define MaxSize 10
 #define ElemType int
 
@@ -11,9 +9,46 @@ typedef struct
 
 void InitSqList(SqList &L)
 {
-  for (int i = 0; i < MaxSize; i++)
-  {
-    L.data[i] = i;
+  L.length = 0;
+}
+
+bool ListInsert(SqList &L, int i, ini e) {
+  if (i < 1 || i > L.length + 1) {
+    return false;
   }
-  L.length = MaxSize;
+  if (L.length >= MaxSize) {
+    return false;
+  }
+  for (int j = L.length; j >= i; j--) {
+    L.data[j] = L.data[j - 1];
+  }
+  L.data[i - 1] = e;
+  L.length++;
+  return true;
+}
+
+bool ListDelete(SqList &L, int i, int &e) {
+  if (i < 1 || i > L.length + 1) {
+    return false;
+  }
+  e = L.data[i - 1];
+  for (int j = i - 1; j < L.length - 1; j++)
+  {
+    L.data[j] = L.data[j + 1];
+  }
+  L.length--;
+  return true;
+}
+
+ElemType GetElem(SqList &L, int i) {
+  return L.data[i - 1];
+}
+
+int Locate(SqList &L, int ElemType e) {
+  for (int i = 0; i < L.length; i++) {
+    if (L.data[i] == e) {
+      return i + 1;
+    }
+  }
+  return 0;
 }
